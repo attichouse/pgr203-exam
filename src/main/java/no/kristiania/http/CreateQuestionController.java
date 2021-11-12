1bd887c8877ad2dd31a3cc7875d17882a820895d
attichouse <77102070+attichouse@users.noreply.github.com>
2021-11-12 13:34:42 +0100

    Fixed semantic setup in html files

--- src/main/java/no/kristiania/http/CreateQuestionController.java
+++ src/main/java/no/kristiania/http/CreateQuestionController.java
@@ -6,19 +6,19 @@ import no.kristiania.survey.QuestionDao;
 import java.sql.SQLException;
 import java.util.Map;
 
-public class CreateQuestionController implements HttpController{
+public class CreateQuestionController implements HttpController {
+
     private final QuestionDao questionDao;
     public CreateQuestionController(QuestionDao questionDao){
         this.questionDao = questionDao;
     }
-
 
     @Override
     public HttpMessage handle(HttpMessage request) throws SQLException {
         Map<String, String> queryMap = HttpMessage.parseRequestParameters(request.messageBody);
         Question question = new Question();
         question.setQuestionDescription(queryMap.get("question"));
-        question.setQuestionIdFk(Long.parseLong(queryMap.get("survey")));
+        question.setQuestionIdFk(Long.parseLong(queryMap.get("surveyName")));
         questionDao.save(question);
 
         return new HttpMessage("HTTP/1.1 200 ok", "It is done");
