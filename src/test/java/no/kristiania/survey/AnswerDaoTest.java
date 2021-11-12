6df6591ddf96799af9ef893848666c9971305b56
attichouse <77102070+attichouse@users.noreply.github.com>
2021-11-12 16:04:57 +0100

    shouldRetrievedSavedAnswers() - passing

--- src/test/java/no/kristiania/survey/AnswerDaoTest.java
+++ src/test/java/no/kristiania/survey/AnswerDaoTest.java
@@ -2,16 +2,20 @@ package no.kristiania.survey;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
-
 import java.sql.SQLException;
 
 public class AnswerDaoTest {
 
+    private SurveyDao surveyDao = new SurveyDao(TestData.testDataSource());
     private QuestionDao questionDao = new QuestionDao(TestData.testDataSource());
     private AnswerDao answerDao = new AnswerDao(TestData.testDataSource());
 
     @Test
     void shouldRetrieveSavedAnswers() throws SQLException {
+        Survey exampleSurvey = new Survey();
+        exampleSurvey.setSurveyName("Wut");
+        surveyDao.save(exampleSurvey);
+
         Question exampleQuestion = new Question();
         exampleQuestion.setQuestionDescription("What is your favourite colour?");
         exampleQuestion.setQuestionIdFk(1);
